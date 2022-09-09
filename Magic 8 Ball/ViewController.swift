cfc850916a668dcf05bf7592d2111cd01f72b4ba
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2022-09-09 14:18:03 +0300

    ask me.ios
    my first IOS APP

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelTxtView: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var btnAsk: UIButton!
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBAction func btnPressed(_ sender: Any) {
+        imgView.image = ballArray[Int.random(in: 0...4)]
+        
+    }
+    
 }
 
